2c99e8c610be6175ae7f1035a4215e0f2d97a9a0
Kai <orozobekov.kai@gmail.com>
2023-10-01 21:17:55 -0400

    fix: no expect error directive

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
           </div>
           <span className="w-full h-[0.5px] bg-creamy"> </span>
         </section>
-        {/* @ts-expect-error Server Component */}
+
         <ToolkitSection />
       </div>
 
